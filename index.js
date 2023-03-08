b4fa7caf40380bee4af295fe7487d2faffed5924
Miguel <miguelitorruiz@gmail.com>
2023-03-08 14:34:31 +0100

    Aplicado certificado y creado .nvmrc

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
 
 //configuracion
 app.set('port', 3000)
+const options = {
+    key: fs.readFileSync('client-key.pem'),
+    cert: fs.readFileSync('client-cert.pem')
+}
 
 //middleware
 app.use(express.static(path.join(__dirname, 'pages')))
@@ -13,6 +20,10 @@ app.get('/', (request, response)=>{
     response.send('Bienvenido')
 })
 
-app.listen(app.get('port'), ()=>{
-    console.log(`Aplicacion ejecutandose en el puerto ${app.get('port')}`)
-})
+// app.listen(app.get('port'), ()=>{
+//     console.log(`Aplicacion ejecutandose en el puerto ${app.get('port')}`)
+// })
+
+//lo demas
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
